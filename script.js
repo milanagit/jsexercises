e5b60e1fdca1599a7a7bd0eb79ff2d9cd16c838d
Milana Kovacevic <miki@192.168.1.6>
2025-12-02 15:44:54 +0100

    added current day and time functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+// fatching DOM elements
+const getCurrentDayAndTimeBtn = document.getElementById('get-current-day-and-time');
+const currentDayAndTimeDisplay = document.getElementById('current-day-and-time');
+
+const currentDayAndTime = () => {
+    const daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let today = new Date();
+    let dayNumber = today.getDay();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+
+    if (hour <= 12) {
+        hour = hour + ' AM';
+    } else {
+        hour =  (hour === 24) ? `0 AM` : `${hour - 12} PM`;
+    }
+
+    // display date and time
+    currentDayAndTimeDisplay.innerHTML = `Today is : ${daylist[dayNumber]}. Current time is : ${hour} : ${minute} : ${second}`;
+};
+
+getCurrentDayAndTimeBtn.addEventListener('click', currentDayAndTime);
